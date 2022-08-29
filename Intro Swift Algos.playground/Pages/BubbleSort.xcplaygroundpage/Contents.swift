8c096c278645af2620a0806df5bdcf5420762f26
Serrayon <serrayon81@gmail.com>
2022-08-28 21:24:02 -0700

    changed the function name

--- Intro Swift Algos.playground/Pages/BubbleSort.xcplaygroundpage/Contents.swift
+++ Intro Swift Algos.playground/Pages/BubbleSort.xcplaygroundpage/Contents.swift
@@ -1,23 +1,21 @@
 import UIKit
 
 class BubbleSort {
-    func sort(_ array: [Int]) -> [Int] {
+    func bubSort( _ array: [Int]) -> [Int] {
         var arr = array
         let n = arr.count
         for i in 0..<n-1 {
             for j in 0..<n-i-1 {
                 if arr[j] > arr[j+1] {
-                    // swap
+                    //swap
                     let temp = arr[j]
                     arr[j] = arr[j+1]
                     arr[j+1] = temp
                 }
             }
         }
-        
         return arr
     }
 }
-
 let bubbleSort = BubbleSort()
-bubbleSort.sort([5, 4, 3, 2, 1])
+bubbleSort.bubSort([34,78,0,-3,3,2,1])
